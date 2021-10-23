2f5407812a9f99944c3bc883ed4660a94df3d4e9
Levi <levilimabr@gmail.com>
2021-10-23 01:20:16 -0500

    add animated vetor

---
+++ mouse-pos.js
@@ -0,0 +1,11 @@
+// script that detects the mouse position and sets the variables in the CSS like (--mouseX and --mouseY)
+
+const root = document.documentElement;
+
+document.addEventListener('mousemove', evt => {
+  let x = evt.clientX / innerWidth;
+  let y = evt.clientY / innerHeight;
+
+  root.style.setProperty('--mouse-x', x);
+  root.style.setProperty('--mouse-y', y);
+});
